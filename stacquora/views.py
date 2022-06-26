d08d651da73eb1d625572829ec555f9214c5cff7
Aswinraaj23 <aswinraaj26@gmail.com>
2022-06-26 11:09:05 +0530

    Create urls, dummy templates, view to check the flow

--- stacquora/views.py
+++ stacquora/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
+def home(request):
+    context = {
+        'title':'hello there'
+    }
+    return render(request, 'stacquora/home.html', context)
