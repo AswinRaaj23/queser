5836f98fb94091d5202e433094f28f8f64e641f6
Aswinraaj23 <aswinraaj26@gmail.com>
2022-06-26 10:36:39 +0530

    Create models for QA and register it with admin

--- stacquora/models.py
+++ stacquora/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField(null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    answer = models.TextField(null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.answer
