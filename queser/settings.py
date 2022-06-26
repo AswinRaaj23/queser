4fba1b35c1d8c0dfd61d802ad2a3b40689173c19
Aswinraaj23 <aswinraaj26@gmail.com>
2022-06-26 12:56:38 +0530

    Add basic styling

--- queser/settings.py
+++ queser/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stacquora'
+    'stacquora',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
