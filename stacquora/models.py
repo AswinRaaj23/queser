a85b1c5e9957b71f4046930e1f44509a9e609356
Aswinraaj23 <aswinraaj26@gmail.com>
2022-06-30 17:26:35 +0530

    Use django-model-utils for models that use time

--- stacquora/models.py
+++ stacquora/models.py
@@ -1,46 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from model_utils.models import TimeStampedModel
 
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
-class Question(models.Model):
+class Question(TimeStampedModel):
     title = models.CharField(max_length=250)
     description = models.TextField(null=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     tags = TaggableManager()
-    
+
     def __str__(self):
         return self.title
 
-class Answer(models.Model):
+class Answer(TimeStampedModel):
     user = models.ForeignKey(User, on_delete = models.CASCADE, null=False)
     question = models.ForeignKey(Question, on_delete=models.CASCADE, null=False, related_name='answers')
     answer = models.TextField(null=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.answer
 
-class QuestionComment(models.Model):
+class QuestionComment(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null= False)
     question = models.ForeignKey(Question, on_delete=models.CASCADE, null=False, related_name='questioncomment')
     comment = models.CharField(max_length=300)
-    created = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.comment
 
-class AnswerComment(models.Model):
+class AnswerComment(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null= False)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=False, related_name='answercomment')
     comment = models.CharField(max_length=300)
-    created = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.comment
